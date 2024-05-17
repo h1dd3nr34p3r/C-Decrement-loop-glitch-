961a76a10b4e5d9af7179fea5fe81ab9b34ae10d
h1dd3nr34p3r <142089254+h1dd3nr34p3r@users.noreply.github.com>
2024-05-17 18:01:12 +0930

    Create main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+int main()
+{
+    int count = 10;
+    for(count = 0; count <= 10; count--)
+        {
+                if(count == 10)
+                break;
+        }
+        return 0;
+}
